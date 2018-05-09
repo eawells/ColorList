9564d86311f72ac71ec23c20ed819b72b1e2bf35
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2018-05-09 13:03:26 -0500

    Added fake buttons

--- App.js
+++ App.js
@@ -7,11 +7,21 @@ import {
 } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      backgroundColor: 'blueviolet'
+    }
+  }
   render() {
+    const { backgroundColor } = this.state
     return (
-      <View style={styles.container}>
+      <View style={[styles.container,{backgroundColor}]}>
+        <Text style={styles.button}>Red</Text>
+        <Text style={styles.button}>Green</Text>
+        <Text style={styles.button}>Blue</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -21,5 +31,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  button: {
+    fontSize: 30,
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    textAlign: 'center'
+
   }
-});
+})
